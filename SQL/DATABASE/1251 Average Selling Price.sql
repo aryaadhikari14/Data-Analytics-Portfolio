e12b2daff172011a76e91530eec1a6d28b5fe70f
aryaadhikari14 <adhikariarya060@gmail.com>
2026-07-12 19:05:07 +0530

    Add comments for Leetcode 1251 SQL query
    Added comments to clarify the purpose and context of the SQL query.

---
+++ SQL/DATABASE/1251 Average Selling Price.sql
@@ -0,0 +1,16 @@
+# Leetcode 1251: Average selling price
+# Difficulty : Easy
+# Database : Mysql
+  
+SELECT P.product_id,
+ROUND(
+    IFNULL(SUM(P.price *
+    u.units) / SUM(u.units), 0),
+    2
+) AS average_price
+FROM Prices p
+LEFT JOIN UnitsSold u
+ON p.product_id = u.product_id
+AND u.purchase_date BETWEEN
+p.start_date AND p.end_date
+GROUP BY p.product_id;
