6dbd8c1ebc40561f7ef78f7e05cfb2d31ce8ac67
aryaadhikari14 <adhikariarya060@gmail.com>
2026-07-28 18:53:54 +0530

    Add SQL query for customers with no transactions
    This SQL query retrieves customer IDs who visited but did not make any transactions, counting the number of such visits.

---
+++ SQL/DATABASE/1581_Customer who visited but did not make any transactions.sql
@@ -0,0 +1,15 @@
+# Leetcode 1581: Customer who visited but did not make any transactions
+# Difficulty : Easy
+# Database : Mysql
+  
+SELECT 
+    customer_id,
+    COUNT(*) AS count_no_trans
+FROM Visits
+LEFT JOIN Transactions
+ON Visits.visit_id =
+Transactions.visit_id
+WHERE 
+Transactions.transaction_id IS 
+NULL
+GROUP BY customer_id;
